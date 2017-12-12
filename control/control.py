4994ff5ade6b5ad656d3ad796ebe8ce5478347c3
olgavedenina <31884507+olgavedenina@users.noreply.github.com>
2017-12-12 15:04:24 +0300

    test

--- control/control.py
+++ control/control.py
@@ -1,18 +1,9 @@
 with open("Extinct_languages.txt", encoding="utf-8") as f:
     lines = f.readlines()
-    #for line in lines:        
-        #if len(line)>35:
-            #print(line)
+    for line in lines:        
+        if len(line)>35:
+            print(line)
    
-    
-    
-    a = 0
-    b = "Critically endangered"
-    for line in f:
-        for word in line.split("\t"):
-            if word[-1] == b:
-                a += 1
-    print(a)
             
     
         
