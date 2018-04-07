f36e7659ad913f3fc27a453d1dbdfb2c2416aed7
olgavedenina <31884507+olgavedenina@users.noreply.github.com>
2018-04-07 11:21:39 +0300

    а теперь неожиданно все работает

--- Test/task 3.py
+++ Test/task 3.py
@@ -1,8 +1,17 @@
 import re
 
 with open('mystem.xml', encoding = 'utf-8') as f:
-    text = f.read
+    text = f.read()
 
-adj = re.findall(r'gr="A.*?жен.*?"', text)
+f_adj = re.findall(r'gr="A=.*?жен.*?" />(.*)</w>', text)
 
-print(adj)
+print(f_adj)
+
+adj_edit = ', '.join(f_adj)
+
+new_file = input('Введите имя файла, в который будет сохранена информация:   ')
+
+with open(new_file, 'w', encoding = 'utf-8') as f:
+    f.write(adj_edit)
+  
+print('Информация записана. Спасибо, что воспользовались программой!')
